2fc65d2cecab58c83c76701ef3931f74c975d038
Ryan Ross <ryan@reross.com>
2022-04-26 11:11:49 -0500

    mixing it up with mixins

--- classes.ts
+++ classes.ts
@@ -1,39 +1,61 @@
-import * as Interfaces from './interfaces';
+import * as Interfaces from "./interfaces";
 
-class UniversityLibrarian implements Interfaces.Librarian {
-    
-    name: string;
-    email: string;
-    department: string;
-    
-    assistCustomer(custName: string) {
-        console.log(this.name + ' is assisting ' + custName);
-    }
+class Employee {
+  title: string;
+
+  addToSchedule(): void {
+    console.log("Employee added to schedule");
+  }
+
+  logTitle(): void {
+    console.log(`Employee has the title: ${this.title}`);
+  }
+}
+
+class Researcher {
+  doResearch(topic: string): void {
+    console.log(`Doing research on: ${topic}`);
+  }
+}
+
+class UniversityLibrarian
+  implements Interfaces.Librarian, Employee, Researcher
+{
+  name: string;
+  email: string;
+  department: string;
+
+  assistCustomer(custName: string) {
+    console.log(this.name + " is assisting " + custName);
+  }
+  title: string;
+  addToSchedule(): void {}
+  logTitle(): void {}
+  doResearch(topic: string): void {}
 }
 
 abstract class ReferenceItem {
-    
-    private _publisher: string;
-    static department: string = 'Research';
+  private _publisher: string;
+  static department: string = "Research";
 
-    constructor(public title: string, protected year: number) {
-        console.log('Creating a new ReferenceItem...');
-    }
-    
-    printItem(): void {
-        console.log(`${this.title} was published in ${this.year}.`);
-        console.log(`Department: ${ReferenceItem.department}`);
-    }
-    
-    get publisher(): string {
-        return this._publisher.toUpperCase();
-    }
-    
-    set publisher(newPublisher: string) {
-        this._publisher = newPublisher;
-    }
-    
-    abstract printCitation(): void;
+  constructor(public title: string, protected year: number) {
+    console.log("Creating a new ReferenceItem...");
+  }
+
+  printItem(): void {
+    console.log(`${this.title} was published in ${this.year}.`);
+    console.log(`Department: ${ReferenceItem.department}`);
+  }
+
+  get publisher(): string {
+    return this._publisher.toUpperCase();
+  }
+
+  set publisher(newPublisher: string) {
+    this._publisher = newPublisher;
+  }
+
+  abstract printCitation(): void;
 }
 
-export { UniversityLibrarian, ReferenceItem };
+export { UniversityLibrarian, ReferenceItem, Employee, Researcher };
