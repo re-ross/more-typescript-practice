492193bbc6b7675a7371d62061b1fcbd2cba5038
Ryan Ross <ryan@reross.com>
2022-04-27 13:16:41 -0500

    practiced promises

--- app.ts
+++ app.ts
@@ -286,3 +286,30 @@ import "./LibrarianExtension";
 // console.log("Beginning search...");
 // getBooksByCategory(Category.Fiction, logCategorySearch);
 // console.log("Search submitted...");
+
+// ~PROMISES~
+
+function getBooksByCategory(cat: Category): Promise<string[]> {
+  let p: Promise<string[]> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let foundBooks: string[] = util.GetBookTitlesByCategory(cat);
+
+      if (foundBooks.length > 0) {
+        resolve(foundBooks);
+      } else {
+        reject("No books found for that category.");
+      }
+    }, 2000);
+  });
+  return p;
+}
+
+console.log("Beginning search...");
+getBooksByCategory(Category.Fiction)
+  .then((titles) => {
+    console.log(`Found titles: ${titles}`);
+    return titles.length;
+  })
+  .then((numOfBooks) => console.log(`Number of books found: ${numOfBooks}`))
+  .catch((reason) => console.log(`Error: ${reason}`));
+console.log("Search submitted...");
