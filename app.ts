f1ba8a71fda5b6114f7036a6a608649da59049d9
Ryan Ross <ryan@reross.com>
2022-04-27 13:22:42 -0500

    refreshed on async / wait with typescript

--- app.ts
+++ app.ts
@@ -289,6 +289,32 @@ import "./LibrarianExtension";
 
 // ~PROMISES~
 
+// function getBooksByCategory(cat: Category): Promise<string[]> {
+//   let p: Promise<string[]> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let foundBooks: string[] = util.GetBookTitlesByCategory(cat);
+
+//       if (foundBooks.length > 0) {
+//         resolve(foundBooks);
+//       } else {
+//         reject("No books found for that category.");
+//       }
+//     }, 2000);
+//   });
+//   return p;
+// }
+
+// console.log("Beginning search...");
+// getBooksByCategory(Category.Fiction)
+//   .then((titles) => {
+//     console.log(`Found titles: ${titles}`);
+//     return titles.length;
+//   })
+//   .then((numOfBooks) => console.log(`Number of books found: ${numOfBooks}`))
+//   .catch((reason) => console.log(`Error: ${reason}`));
+// console.log("Search submitted...");
+
+// ~ASYNC / AWAIT~
 function getBooksByCategory(cat: Category): Promise<string[]> {
   let p: Promise<string[]> = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -304,12 +330,10 @@ function getBooksByCategory(cat: Category): Promise<string[]> {
   return p;
 }
 
+async function logSearchResults(bookCategory: Category) {
+  let foundBooks = await getBooksByCategory(bookCategory);
+  console.log(foundBooks);
+}
 console.log("Beginning search...");
-getBooksByCategory(Category.Fiction)
-  .then((titles) => {
-    console.log(`Found titles: ${titles}`);
-    return titles.length;
-  })
-  .then((numOfBooks) => console.log(`Number of books found: ${numOfBooks}`))
-  .catch((reason) => console.log(`Error: ${reason}`));
+logSearchResults(Category.Fiction).catch((reason) => console.log(reason));
 console.log("Search submitted...");
